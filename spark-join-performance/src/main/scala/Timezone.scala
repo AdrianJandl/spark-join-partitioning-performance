0e8d58870f146b531b7b7ae685581b5e7eb7e000
Adrian Jandl <adrian.jandl@gmail.com>
2018-11-20 22:12:50 +0100

    Adds code for big/small dataset

---
+++ spark-join-performance/src/main/scala/Timezone.scala
@@ -0,0 +1,5 @@
+case class Timezone(country_code: String,
+                    TimeZoneId: String,
+                    GMT_offset: String,
+                    DST_offset: String,
+                    rawOffset: Double)
